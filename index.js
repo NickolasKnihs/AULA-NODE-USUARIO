2f47d69a7befc7ad7f602fd49bcdfa5334aff684
NickolasKnihs <160547042+NickolasKnihs@users.noreply.github.com>
2024-02-29 19:53:48 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 // Criar API
 
-const espresso = require('express'); //importar codigo express
+const espresso = require('express'); //importar codigo express (para fazer API Js)
 const meuServidor = espresso();      //executar a função  
-meuServidor.use(espresso.json()); //informar uso do JSON
+meuServidor.use(espresso.json());    //informar uso do JSON, informando que sera via json
 
-const usuarios = ['Nickolas'];
+//lista de usuarios (representa um banco de dados)
+const usuarios = [
+    {
+        nome: 'Nickolas', 
+        idade: 27,
+        CPF: '10139768947'
+    }
+];       
 
-meuServidor.get('/minhaAulaDeNode', (requisicao, resposta) => {
+// configurando rota
+meuServidor.get('/minhaAulaDeNode', (requisicao, resposta) => {   
     console.log(requisicao.body);
     let respostaUsuarios = '';
     for (let index = 0; index < usuarios.length; index ++){
+        const usuario = usuarios[index];
         respostaUsuarios += '<p>';
-        respostaUsuarios += usuarios[index];
+        respostaUsuarios += 'Nome:'
+        respostaUsuarios += usuario.nome;
+        respostaUsuarios += '</br>Idade:'
+        respostaUsuarios += usuario.idade;
+        respostaUsuarios += '</br>CPF:'
+        respostaUsuarios += usuario.CPF;
         respostaUsuarios += '</p>';
     }
     resposta.send(respostaUsuarios);
 });
 
 // Instalar thunder Client
+//cadastro
 meuServidor.post('/cadastraMinhaAulaDeNode', (requisicao, resposta) =>{
-    console.log(requisicao.body);
+    //console.log(requisicao.body);
     usuarios.push(requisicao.body.nome);
     resposta.send();
 });
 
+// thunder
+// body:
+/*
+    {
+        "nome": "Pedro"
+    }
+    */
+
+
+
+
+
+// deve ser sempra a ultima funcao do arquivo
 meuServidor.listen(4300, () =>{
     console.log('Meu primeiro servidor na porta 4300.');
 })
