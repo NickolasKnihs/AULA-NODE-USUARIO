c384dda3a66eea513b9b276c6327ace0e30b483c
NickolasKnihs <160547042+NickolasKnihs@users.noreply.github.com>
2024-02-29 21:49:50 -0300

    Add files via upload
    aula 29/02

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const meuServidor = espresso();      //executar a função
 meuServidor.use(espresso.json());    //informar uso do JSON, informando que sera via json
 
 //lista de usuarios (representa um banco de dados)
-const usuarios = [
-    {
+const listaUsuarios = [
+    {   
+        id: 1,
         nome: 'Nickolas', 
         idade: 27,
         CPF: '10139768947'
@@ -14,17 +15,19 @@ const usuarios = [
 ];       
 
 // configurando rota
-meuServidor.get('/minhaAulaDeNode', (requisicao, resposta) => {   
+meuServidor.get('/usuarios', (requisicao, resposta) => {   
     console.log(requisicao.body);
     let respostaUsuarios = '';
-    for (let index = 0; index < usuarios.length; index ++){
-        const usuario = usuarios[index];
+    for (let index = 0; index < listaUsuarios.length; index ++){
+        const usuario = listaUsuarios[index];
         respostaUsuarios += '<p>';
-        respostaUsuarios += 'Nome:'
+        respostaUsuarios += 'Código: ';
+        respostaUsuarios += usuario.id;
+        respostaUsuarios += '</br>Nome: '
         respostaUsuarios += usuario.nome;
-        respostaUsuarios += '</br>Idade:'
+        respostaUsuarios += '</br>Idade: '
         respostaUsuarios += usuario.idade;
-        respostaUsuarios += '</br>CPF:'
+        respostaUsuarios += '</br>CPF: '
         respostaUsuarios += usuario.CPF;
         respostaUsuarios += '</p>';
     }
@@ -32,10 +35,29 @@ meuServidor.get('/minhaAulaDeNode', (requisicao, resposta) => {
 });
 
 // Instalar thunder Client
-//cadastro
-meuServidor.post('/cadastraMinhaAulaDeNode', (requisicao, resposta) =>{
+//cadastro NO THUNDER
+meuServidor.post('/usuarios', (requisicao, resposta) =>{
     //console.log(requisicao.body);
-    usuarios.push(requisicao.body.nome);
+    const nome = requisicao.body.nome; 
+    const idade = requisicao.body.idade; 
+    const cpf = requisicao.body.cpf; 
+    let codigo = -99999999999999999999999999;
+    for (let index = 0; index < listaUsuarios.length; index++){
+        const usuarioAtual = listaUsuarios[index];
+        if (usuarioAtual.id > codigo){
+            codigo = usuarioAtual.id;
+        }
+    }
+    if (codigo < 0){
+        codigo = 0;
+    }
+    const novoUsuario = {
+        id: codigo + 1,
+        nome: nome,
+        idade: idade,
+        CPF: cpf
+    }
+    listaUsuarios.push(novoUsuario); // ADICIONAR A USUARIOS
     resposta.send();
 });
 
@@ -47,8 +69,20 @@ meuServidor.post('/cadastraMinhaAulaDeNode', (requisicao, resposta) =>{
     }
     */
 
+    //atualização:
+    meuServidor.put('/usuarios/:usuarioId', (requisicao, resposta) => {
+        const codigoUsuario = requisicao.params.usuarioId;
+        const usuarioEncontrado = listaUsuarios.find((usuarioAtual) => usuarioAtual.id == codigoUsuario);
+        console.log(usuarioEncontrado);
 
-
+        const nome = requisicao.body.nome;
+        const idade = requisicao.body.idade;
+        const cpf = requisicao.body.cpf;
+        usuarioEncontrado.nome = nome;
+        usuarioEncontrado.idade = idade;
+        usuarioEncontrado.CPF = cpf;
+        resposta.send();
+    });
 
 
 // deve ser sempra a ultima funcao do arquivo
